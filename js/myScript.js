49d259994034102e10f2cfd52f3e20c903247c10
Brianengine <brianduke0097@gmail.com>
2024-05-22 16:32:05 +0800

    新增JS新增JS

--- js/myScript.js
+++ js/myScript.js
@@ -18,14 +18,3 @@ document.getElementsByClassName("tablink")[0].click();
 
 
 // 新增
-var modalElem = document.getElementById("tyModal");
-        var spanElem = document.getElementsByClassName("tyClose")[0];
-
-        spanElem.onclick = function () {
-            modalElem.style.display = "none";
-        }
-
-        setTimeout(function () {
-            modalElem.style.display = "block";
-        }, 1000);
-    
